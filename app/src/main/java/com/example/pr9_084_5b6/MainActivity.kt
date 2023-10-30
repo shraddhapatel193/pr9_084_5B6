845e1ced956c8d8683ecf2398ef265dcbf05d6fe
shraddha <shraddhabpatel21@gnu.ac.in>
2023-10-30 22:17:58 +0530

    pr9

---
+++ app/src/main/java/com/example/pr9_084_5b6/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pr9_084_5b6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "pr9_084_5B6"
+include(":app")
+ 
